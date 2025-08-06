a39ed8bc60186b2ea444c03f00ec0c320bd9c038
Thanik30 <jeng.thanik@gmail.com>
2025-08-06 16:49:30 +0700

    lab09 completed

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -8,19 +8,17 @@ export default function MainLayout() {
       <div className="row h-100">
         <div className="col-2 col-md-2 p-0">
           {/* Sidebar ซ้าย */}
-          <Sidebar userName="chanadda" type="admin" />
+          <Sidebar userName="Thanik" type="student" />
         </div>
         <div className="col-10 col-md-10 p-0">
           {/* Header ด้านบน */}
           <Header />
-
           {/* เนื้อหาหลัก */}
           <main className="flex-grow-1 p-4 min-vh-100">
             <Outlet />
           </main>
-
           {/* Footer ด้านล่าง */}
-          <Footer year="2026" fullName="chanadda" studentId="67062039" />
+          <Footer year="2026" fullName="Thanik Sirithon" studentId="670610704" />
         </div>
       </div>
     </div>
--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
 export default function CounterPage() {
+  const [counter , CounterSet] = useState(0)
+  const Clicked = () => {
+    CounterSet(counter + 1);
+  }
   return (
     <div className="container text-center">
       <h2>Counter Page</h2>
       {/* Result Text */}
-      <p>Counter:</p>
-      <button>Increase</button>
+      <p>Counter : {counter}</p>
+      <button onClick={() => Clicked()}>Increase</button>
     </div>
   );
 }
